0ec0a932ec3d48c46ecf64931e81657510e9dd9a
NITHI2209 <nithyasree065@gmail.com>
2025-07-24 19:40:32 +0530

    Update 5.c&o.py

--- 5.c&o.py
+++ 5.c&o.py
@@ -8,15 +8,15 @@ class mental_hos:
         self.called = True
         return f"The paitient is mentally unstable assuming by ppl around,{checking}"
     
-    def dr(self,dr_check):
+    def dr(self,dr_check,prescribtion):
         self.has_called = True
         if self.called:
-            return f"Report by doctor,{dr_check}"
+            return f"Report by doctor,{dr_check} and prescribed,{prescribtion}"
         else:
             return "First check the behaviour of the paitent by ppl around and then take to hospital"
-    def medical_facility(self,suggestion):
+    def medical_facility(self,suggestion,medication,time):
         if self.called and self.has_called:
-            return f"The doctor suggest to {suggestion}"
+            return f"The doctor suggest to {suggestion}, medication,{medication} and duration of time to cure {time}"
         else:
             if self.called:
                 return "Consult with the dr first"
@@ -27,5 +27,5 @@ class mental_hos:
             
 paitent = mental_hos()
 print(paitent.check("yes"))
-print(paitent.dr("yes"))
-print(paitent.medical_facility("dr suugest to admit"))
+print(paitent.dr("yes","prescribed to neurologist"))
+print(paitent.medical_facility("dr suugest to admit","tablets and meditation","6 months"))
