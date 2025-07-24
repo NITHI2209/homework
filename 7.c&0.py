8db65dfaae4a84fdfa81fcbc58f098045c886603
NITHI2209 <nithyasree065@gmail.com>
2025-07-24 19:51:59 +0530

    Update 7.c&0.py

--- 7.c&0.py
+++ 7.c&0.py
@@ -4,20 +4,20 @@ class parlour:
         self.called = False
         self.has_called = False 
 
-    def facial(self,enter_the_facial,price):
+    def facial(self,enter_the_facial,price,duration):
         self.called = True
-        return f"Facial:{enter_the_facial} and price:{price}"
+        return f"Facial:{enter_the_facial} ,price:{price} and duration:{duration}"
     
-    def threading(self,type,price_for_threading):
+    def threading(self,type,price_for_threading,duration):
         self.has_called = True 
         if self.called:
-            return f"Type for threading:{type} , price for threading:{price_for_threading}"
+            return f"Type for threading:{type} , price for threading:{price_for_threading},duration:{duration}"
         else:
             return "First do facial" 
         
-    def upper_hair(self,type,price_for_upper_hair):
+    def upper_hair(self,type,price_for_upper_hair,time):
         if self.called and self.has_called:
-            return f"Type for upper hair:{type}, price:{price_for_upper_hair}"
+            return f"Type for upper hair:{type}, price:{price_for_upper_hair}, time:{time}"
         else:
             if self.called:
                 return "Do threading then do upper hair"
@@ -27,6 +27,6 @@ class parlour:
                 return "Do facial and threading first"
             
 customer = parlour()
-print(customer.facial("skin brightining",1500))
-print(customer.threading("Thread",45))
-print(customer.upper_hair("wax",80))
+print(customer.facial("skin brightining",1500,"1hr"))
+print(customer.threading("Thread",45,"3mins"))
+print(customer.upper_hair("wax",80,"5mins"))
